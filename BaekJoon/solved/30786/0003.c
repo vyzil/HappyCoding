b7ca8a6045598d55000a772efb9bd97214de582a
vyzil <vyzil712@gmail.com>
2024-06-17 17:30:26 +0900

    solve 30786

---
+++ BaekJoon/solved/30786/0003.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef struct _POINT{
+    int x, y;
+} POINT;
+
+int main(void){
+    int i;
+    int N;
+    int dist, idx;
+    POINT prev, cur;
+
+    scanf("%d", &N);
+    scanf("%d %d", &cur.x, &cur.y);
+    idx = 0;
+    for(i = 0; i < N-1; i++){
+        prev.x = cur.x; prev.y = cur.y;
+        scanf("%d %d", &cur.x, &cur.y);
+        dist  = (cur.x > prev.x ? cur.x - prev.x : prev.x - cur.x);
+        dist += (cur.y > prev.y ? cur.y - prev.y : prev.y - cur.y);
+
+        if(dist % 2) {
+            idx = i+1;
+            break;
+        }
+    }
+
+    
+    if(idx){
+        printf("YES\n");
+        printf("%d ", idx);
+        for(i = 1; i <= N; i++){
+            if(i != idx && i != idx+1){
+                printf("%d ", i);
+            }
+        }
+        printf("%d\n", idx+1);
+    }
+    else printf("NO\n");
+
+    return 0;
+}
